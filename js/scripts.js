3c06c80faf3e7ea9b0358925765f5177beb68187
tinashegufu <49593281+tinashegufu@users.noreply.github.com>
2020-04-17 03:31:27 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,7 @@ function validate(){
     if(name.length < 2){
         text="Enter more than 1 character for name";
         error_message.innerHTML = text;
+        alert(text);
         return false;
 
     }
@@ -20,28 +21,33 @@ function validate(){
     if(!isNaN(name)){
         text="Name cannot be digits";
         error_message.innerHTML = text;
+        alert(text);
         return false;
     }
 
     if(number.length < 9){
       text = "Please Enter valid Phone Number";
       error_message.innerHTML = text;
+      alert(text);
       return false;
     }
     if(isNaN(number)){
         text = "Phone Number should be digits only";
         error_message.innerHTML = text;
+        alert(text);
         return false;
       }
 
     if(email.indexOf("@") == -1 || email.length < 6){
       text = "Please Enter valid Email";
       error_message.innerHTML = text;
+      alert(text);
       return false;
     }
     if(message.length <= 10){
       text = "Please Enter More Than 10 Characters In Enquiry field";
       error_message.innerHTML = text;
+      alert(text);
       return false;
     }
 
